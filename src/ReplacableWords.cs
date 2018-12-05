be28f6b659c824b59af724721e528c1b6f6394b7
k3ldar <s1cart3r@gmail.com>
2018-12-05 20:33:18 +0000

    #1 Environment variables, special folders and apppath

---
+++ src/ReplacableWords.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppSettings
+{
+    public enum ReplacableWord
+    {
+        /// <summary>
+        /// Current Application Folder
+        /// </summary>
+        AppPath = 1,
+    }
+}
